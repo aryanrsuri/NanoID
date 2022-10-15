91e69cb004fadef749621b15e6cb566156bda917
Aryan <13823329+aryanrsuri@users.noreply.github.com>
2022-10-15 15:14:27 -0700

    NanoID generator (byte sized)

---
+++ a.ts
@@ -0,0 +1,2 @@
+export const a =
+  "ModuleSymbhasOwnPr-0123456789ABCDEFGHNRVfgctiUvz_KqYTJkLxpZXIjQW";
---
+++ nid.ts
@@ -0,0 +1,9 @@
+import { a } from "./a.ts";
+
+export const nid = (s = 21) => {
+  const bytes: Uint8Array = crypto.getRandomValues(new Uint8Array(s));
+  let id = "";
+
+  while (s--) id += a[bytes[s] & 63];
+  return id;
+};
