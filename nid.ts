7df2f56b6aae36d2b6ae13c243ceef938ad8bc68
Aryan Suri <arysuri@proton.me>
2023-05-09 21:40:14 -0700

    added decleration file

--- nid.ts
+++ nid.ts
@@ -1,9 +1,7 @@
 import { a } from "./a.ts";
-
 export const nid = (s = 21) => {
   const bytes: Uint8Array = crypto.getRandomValues(new Uint8Array(s));
   let id = "";
-
   while (s--) id += a[bytes[s] & 63];
   return id;
 };
